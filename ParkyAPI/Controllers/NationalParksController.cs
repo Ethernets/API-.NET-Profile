44590d1c824a4ef6ce7612c850e83a025f27e713
Ethernets <ethernetss@gmail.com>
2022-06-08 10:55:02 +0400

    Add POST

--- ParkyAPI/Controllers/NationalParksController.cs
+++ ParkyAPI/Controllers/NationalParksController.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ParkyAPI.Models;
 using ParkyAPI.Models.Dtos;
 using ParkyAPI.Repository.IRepository;
 
@@ -31,6 +33,50 @@ namespace ParkyAPI.Controllers
                 objDto.Add(_mapper.Map<NationalParkDto>(obj));
             }
             return Ok(objDto);
+        }
+
+        [HttpGet("{nationalParkId:int}")]
+        public IActionResult GetNationalParks(int nationalParkId)
+        {
+            var obj = _parkRepository.GetNationalPark(nationalParkId);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            var objDto = _mapper.Map<NationalParkDto>(obj);
+            return Ok(objDto);
+        }
+
+        [HttpPost]
+        public IActionResult CreateNationalPark([FromBody] NationalParkDto nationalParkDto)
+        {
+            if (nationalParkDto == null)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (_parkRepository.NationalParkExist(nationalParkDto.Name))
+            {
+              ModelState.AddModelError("", "Национальный парк существует");
+              return StatusCode(404, ModelState);
+            }
+
+            if (!ModelState.IsValid )
+            {
+                return BadRequest(ModelState);
+
+            }
+
+            var nationalParkObj = _mapper.Map<NationalPark>(nationalParkDto);
+
+            if (!_parkRepository.CreateNationalPark(nationalParkObj))
+            {
+                ModelState.AddModelError("", $"Что-то пошло не так при сохранении записи {nationalParkObj.Name}");
+                return StatusCode(500, ModelState);
+            }
+
+            return Ok();
+
         }
     }
 }
