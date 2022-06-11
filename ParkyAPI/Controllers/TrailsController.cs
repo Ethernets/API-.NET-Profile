d69cd0e2f0bda61614cea7b77fc9253b624c5973
Ethernets <ethernetss@gmail.com>
2022-06-11 23:57:23 +0400

    Add new Controller Trails

---
+++ ParkyAPI/Controllers/TrailsController.cs
@@ -0,0 +1,156 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ParkyAPI.Models;
+using ParkyAPI.Models.Dtos;
+using ParkyAPI.Repository.IRepository;
+
+namespace ParkyAPI.Controllers
+{
+    [Route("api/Trails")]
+    [ApiController]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public class TrailsController : ControllerBase
+    {
+        private ITrailRepository _trailRepository;
+        private readonly IMapper _mapper;
+
+        public TrailsController(ITrailRepository trailRepository, IMapper mapper)
+        {
+            _trailRepository = trailRepository;
+            _mapper = mapper;
+        }
+
+        /// <summary>
+        /// Вывод всех тропинок парка
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(List<TrailDto>))]
+        [ProducesResponseType(400)]
+        public IActionResult GetTrails()
+        {
+            var objList = _trailRepository.GetTrails();
+
+            var objDto = new List<TrailDto>();
+
+            foreach (var obj in objList)
+            {
+                objDto.Add(_mapper.Map<TrailDto>(obj));
+            }
+            return Ok(objDto);
+        }
+
+        /// <summary>
+        /// Вывод нужной тропинки по её ID 
+        /// </summary>
+        /// <param name="trailId">Ввод ID трапинки парка</param>
+        /// <returns></returns>
+        [HttpGet("{trailId:int}", Name = "GetTrail")]
+        [ProducesResponseType(200, Type = typeof(TrailDto))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesDefaultResponseType]
+        public IActionResult GetTrails(int trailId)
+        {
+            var obj = _trailRepository.GetTrail(trailId);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            var objDto = _mapper.Map<TrailDto>(obj);
+            return Ok(objDto);
+        }
+
+        /// <summary>
+        /// Создание новых данных для тропинок парка
+        /// </summary>
+        /// <param name="trailDto">Введите нужные данные</param>
+        /// <returns></returns>
+        [HttpPost]
+        [ProducesResponseType(201, Type = typeof(TrailDto))]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult CreateTrail([FromBody] TrailDto trailDto)
+        {
+            if (trailDto == null)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (_trailRepository.TrailExist(trailDto.Name))
+            {
+              ModelState.AddModelError("", "Национальный парк существует");
+              return StatusCode(404, ModelState);
+            }
+
+            if (!ModelState.IsValid )
+            {
+                return BadRequest(ModelState);
+
+            }
+
+            var trailObj = _mapper.Map<Trail>(trailDto);
+
+            if (!_trailRepository.CreateTrail(trailObj))
+            {
+                ModelState.AddModelError("", $"Что-то пошло не так при сохранении записи {trailObj.Name}");
+                return StatusCode(500, ModelState);
+            }
+
+            return CreatedAtRoute("GetTrail", new {trailId = trailObj.Id}, trailObj);
+        }
+
+        /// <summary>
+        /// Обновление данных выбранной тропинки
+        /// </summary>
+        /// <param name="trailId">Введите ID тропинки парка для её обновления</param>
+        /// <returns></returns>
+        [HttpPatch("{trailId:int}", Name = "UpdateTrail")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult UpdateTrail(int trailId, [FromBody] TrailDto trailDto)
+        {
+            if (trailDto == null || trailId != trailDto.Id)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var trailObj = _mapper.Map<Trail>(trailDto);
+
+            if (!_trailRepository.UpdateTrail(trailObj))
+            {
+                ModelState.AddModelError("", $"Что-то пошло не так при обновлении записи {trailObj.Name}");
+                return StatusCode(500, ModelState);
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{trailId:int}", Name = "DeleteTrail")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult DeleteTrail(int trailId)
+        {
+            if (!_trailRepository.TrailExist(trailId))
+            {
+                return NotFound();
+            }
+
+            var trailObj = _trailRepository.GetTrail(trailId);
+
+            if (!_trailRepository.DeleteTrail(trailObj))
+            {
+                ModelState.AddModelError("", $"Что-то пошло не так при удалении записи {trailObj.Name}");
+                return StatusCode(500, ModelState);
+            }
+
+            return NoContent();
+        }
+    }
+}
--- ParkyAPI/Program.cs
+++ ParkyAPI/Program.cs
@@ -13,6 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<INationalParkRepository, NationalParkRepository>();
+builder.Services.AddScoped<ITrailRepository, TrailRepository>();
 
 builder.Services.AddAutoMapper(typeof(ParkyMappings));
 
